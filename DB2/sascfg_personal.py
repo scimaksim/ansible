105a9e118b7f45f9746e28a2ecdd980deb7d2398
Maksim Nikiforov <maksim.ast@gmail.com>
2021-07-28 10:57:35 -0400

    Modified for restores

---
+++ DB2/sascfg_personal.py
@@ -0,0 +1,12 @@
+SAS_config_names=['oda']
+oda = {'java' : '/usr/bin/java',
+#US Home Region
+'iomhost' : ['odaws01-usw2.oda.sas.com','odaws02-usw2.oda.sas.com','odaws03-usw2.oda.sas.com','odaws04-usw2.oda.sas.com'],
+#European Home Region
+#'iomhost' : ['odaws01-euw1.oda.sas.com','odaws02-euw1.oda.sas.com'],
+#Asia Pacific Home Region
+#'iomhost' : ['odaws01-apse1.oda.sas.com','odaws02-apse1.oda.sas.com'],
+'iomport' : 8591,
+'authkey' : 'oda',
+'encoding' : 'utf-8'
+}
